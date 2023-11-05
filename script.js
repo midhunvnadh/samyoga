ab1545daf01efdb6ec5fcca389d1be08310ca996
anantharamanmac <108387642+anantharamanmac@users.noreply.github.com>
2023-11-05 21:04:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// script.js
+window.addEventListener("load", function() {
+    // Hide the loading screen and show the page content
+    document.querySelector(".loading-screen").style.display = "none";
+    document.querySelector(".page-content").style.display = "block";
+});
